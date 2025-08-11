2f0a9e57e205eaa2ed2c76476bb240421589de2f
Troy Rijkaard <troy.rijkaard@epsyhealth.com>
2025-08-11 15:08:35 +0100

    More changes

--- Sources/SegmentSingular/SingularIntegation.h
+++ Sources/SegmentSingular/SingularIntegation.h
@@ -7,7 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <Analytics/SEGIntegration.h>
+@import Segment;
 
 NS_ASSUME_NONNULL_BEGIN
 
--- Sources/SegmentSingular/SingularIntegation.m
+++ Sources/SegmentSingular/SingularIntegation.m
@@ -7,8 +7,8 @@
 //
 
 #import "SingularIntegation.h"
-#import <Analytics/SEGAnalytics.h>
-#import <Singular/Singular.h>
+@import Segment;
+@import Singular;
 
 #define SEGMENT_WRAPPER_NAME @"Segment"
 #define SEGMENT_WRAPPER_VERSION @"1.2.0"
